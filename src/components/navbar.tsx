33371c36197cfb3f6b83f04c61be2770eca3e833
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2022-11-19 19:54:54 +0200

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,7 @@
   ```
 */
 import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
